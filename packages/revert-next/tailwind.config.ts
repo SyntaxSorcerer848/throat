f1db2761e4fc52f47c6e002ed2692427434bb577
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-01-03 08:26:17 +0000

    Optimize dependencies - adding new feature

--- packages/revert-next/tailwind.config.ts
+++ packages/revert-next/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 import colors from 'tailwindcss/colors';
 
-const config: Config = {
+let config: Config = {
     content: [
         './pages/**/*.{js,ts,jsx,tsx,mdx}',
         './components/**/*.{js,ts,jsx,tsx,mdx}',
