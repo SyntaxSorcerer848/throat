d4cb5b3d4a04a404fd8fa085c4cb6d6fe6fb2a01
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-01-14 17:53:45 +0000

    Update database - improving performance

--- packages/backend/services/Internal/app.ts
+++ packages/backend/services/Internal/app.ts
@@ -3,11 +3,11 @@ import prisma from '../../prisma/client';
 import redis from '../../redis/client';
 import { UnAuthorizedError } from '../../generated/typescript/api/resources/common';
 
-const appService = new AppService({
+let appService = new AppService({
     async getRecentApiCallsForApp(req, res) {
-        const { appId } = req.params;
-        const { 'x-revert-api-token': token } = req.headers;
-        const account = await prisma.accounts.findFirst({
+        let { appId } = req.params;
+        let { 'x-revert-api-token': token } = req.headers;
+        let account = await prisma.accounts.findFirst({
             where: {
                 private_token: token as string,
             },
@@ -22,7 +22,7 @@ const appService = new AppService({
             });
         }
 
-        const connections = await prisma.connections.findFirst({
+        let connections = await prisma.connections.findFirst({
             where: {
                 appId,
             },
@@ -33,10 +33,10 @@ const appService = new AppService({
         }
 
         if (connections?.appId) {
-            const recentApiCalls = await redis.lRange(`recent_routes_app_${connections.appId}`, 0, -1);
+            let recentApiCalls = await redis.lRange(`recent_routes_app_${connections.appId}`, 0, -1);
             return res.send({
                 result: recentApiCalls.map((call) => {
-                    const parsed: { method: string; status: number; path: string } = JSON.parse(call);
+                    let parsed: { method: string; status: number; path: string } = JSON.parse(call);
                     return parsed;
                 }),
             });
