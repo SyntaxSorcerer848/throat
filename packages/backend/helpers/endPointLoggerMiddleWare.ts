22b7fdcb04627848a778476459cb9bc685a36597
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-01-25 02:07:20 +0000

    Add build - handling edge cases

--- packages/backend/helpers/endPointLoggerMiddleWare.ts
+++ packages/backend/helpers/endPointLoggerMiddleWare.ts
@@ -4,11 +4,11 @@ import { logError } from './logger';
 import prisma from '../prisma/client';
 import { getDateWithShortMonth } from './timeZoneHelper';
 // @FIXME Add logic for error
-let endpointLogger = () => async (req: Request, res: Response, next: NextFunction) => {
+const endpointLogger = () => async (req: Request, res: Response, next: NextFunction) => {
     try {
-        let path = req.path;
-        let { 'x-revert-api-token': token, 'x-revert-t-id': tenantId } = req.headers;
-        let toAllow =
+        const path = req.path;
+        const { 'x-revert-api-token': token, 'x-revert-t-id': tenantId } = req.headers;
+        const toAllow =
             path.includes('/crm') ||
             path.includes('/chat') ||
             path.includes('/ticket') ||
@@ -16,17 +16,17 @@ let endpointLogger = () => async (req: Request, res: Response, next: NextFunctio
             path.includes('/accounting');
 
         if (!toAllow) return next();
-        let logEntry: any = {
+        const logEntry: any = {
             method: req.method,
             path: path,
             status: undefined,
         };
 
         if (res.headersSent) logEntry.status = res.statusCode;
-        let queueLength = await redis.lPush(`recent_routes_${token}`, JSON.stringify(logEntry));
+        const queueLength = await redis.lPush(`recent_routes_${token}`, JSON.stringify(logEntry));
         if (queueLength && queueLength > 8) await redis.rPop(`recent_routes_${token}`);
 
-        let environment = await prisma.environments.findFirst({
+        const environment = await prisma.environments.findFirst({
             where: {
                 private_token: String(token),
             },
@@ -38,12 +38,12 @@ let endpointLogger = () => async (req: Request, res: Response, next: NextFunctio
         await redis.INCR(`request_count_${environment.id}`);
 
         // Summary of Api Calls by date for last 7 days + 1 buffer day
-        let key = `summary_api_calls_${environment.id}`;
+        const key = `summary_api_calls_${environment.id}`;
         await redis.hIncrBy(key, getDateWithShortMonth(), 1);
-        let isExpiryExist = await redis.ttl(key); // -1 means expiry doesn't exist, -2 means key doesn't exist
+        const isExpiryExist = await redis.ttl(key); // -1 means expiry doesn't exist, -2 means key doesn't exist
 
         if (isExpiryExist === -1) {
-            let isExpirySet = await redis.expire(key, 60 * 60 * 24 * 8);
+            const isExpirySet = await redis.expire(key, 60 * 60 * 24 * 8);
             if (!isExpirySet) {
                 console.error(isExpirySet);
             }
@@ -51,14 +51,14 @@ let endpointLogger = () => async (req: Request, res: Response, next: NextFunctio
 
         // Recent Api Calls for Particular App
 
-        let connections = await prisma.connections.findFirst({
+        const connections = await prisma.connections.findFirst({
             where: {
                 t_id: tenantId as string,
             },
         });
 
         if (connections?.appId) {
-            let recentAppCalls = await redis.lPush(
+            const recentAppCalls = await redis.lPush(
                 `recent_routes_app_${connections.appId}`,
                 JSON.stringify(logEntry)
             );
