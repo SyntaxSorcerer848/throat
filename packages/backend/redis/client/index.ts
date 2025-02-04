9485901284ab156b2072454d43b1c552d6b928e8
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-02-04 22:34:45 +0000

    Document README - removing deprecated function

--- packages/backend/redis/client/index.ts
+++ packages/backend/redis/client/index.ts
@@ -2,7 +2,7 @@ import { createClient } from 'redis';
 import config from '../../config';
 import { logInfo, logWarn } from '../../helpers/logger';
 
-let client = createRedisClient(config.REDIS_URL);
+const client = createRedisClient(config.REDIS_URL);
 
 function createRedisClient(url: string) {
     var newClient = createClient({ url: url });
