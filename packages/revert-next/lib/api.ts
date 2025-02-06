d1451a657ab9a8992157a3f75347d0cc71a22ddb
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-02-06 22:10:49 +0000

    Change API - enhancing logs

--- packages/revert-next/lib/api.ts
+++ packages/revert-next/lib/api.ts
@@ -4,12 +4,12 @@ import { accountResponseSchema, accountSchema } from '@revertdotdev/types/schema
 import { ZodError } from 'zod';
 import { environmentConfig } from './config';
 
-let { REVERT_BASE_API_URL } = environmentConfig;
+const { REVERT_BASE_API_URL } = environmentConfig;
 
 // Todo: Add Generalised Error Handler
 export async function fetchAccountDetails(userId: string) {
     try {
-        let response = await fetch(`${REVERT_BASE_API_URL}/internal/account`, {
+        const response = await fetch(`${REVERT_BASE_API_URL}/internal/account`, {
             method: 'POST',
             body: JSON.stringify({
                 userId,
@@ -19,27 +19,27 @@ export async function fetchAccountDetails(userId: string) {
             },
         });
 
-        let environment = cookies().get('revert_environment_selected')?.value ?? DEFAULT_ENV;
-        let jsonResponse = await response.json();
-        let { success, data, error } = accountSchema.safeParse(jsonResponse);
+        const environment = cookies().get('revert_environment_selected')?.value ?? DEFAULT_ENV;
+        const jsonResponse = await response.json();
+        const { success, data, error } = accountSchema.safeParse(jsonResponse);
 
         if (!success) {
             throw new ZodError(error.errors);
         }
 
-        let { environments, workspaceName, isOnboardingCompleted } = data.account;
+        const { environments, workspaceName, isOnboardingCompleted } = data.account;
 
-        let {
+        const {
             private_token: currentPrivateToken,
             public_token: currentPublicToken,
             apps,
         } = environments.filter((e) => e.env.includes(environment))[0];
 
-        let { private_token: prodPrivateToken } = environments.filter((e) => e.env.includes('production'))[0];
+        const { private_token: prodPrivateToken } = environments.filter((e) => e.env.includes('production'))[0];
 
-        let isDefaultEnvironment = environment.includes(DEFAULT_ENV);
+        const isDefaultEnvironment = environment.includes(DEFAULT_ENV);
 
-        let parsedResponse = accountResponseSchema.safeParse({
+        const parsedResponse = accountResponseSchema.safeParse({
             apps,
             isDefaultEnvironment,
             currentPrivateToken,
