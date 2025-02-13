b1e2a591deed2c6d67145c69d48329e27d6ebd6b
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-02-13 20:35:48 +0000

    Update database - fixing a bug

--- packages/backend/routes/v1/crm/authHandlers/hubspot.ts
+++ packages/backend/routes/v1/crm/authHandlers/hubspot.ts
@@ -24,15 +24,15 @@ class HubspotAuthHandler extends BaseOAuthHandler {
         redirectUrl,
     }: IntegrationAuthProps) {
         try {
-            let url = 'https://api.hubapi.com/oauth/v1/token';
-            let formData = {
+            const url = 'https://api.hubapi.com/oauth/v1/token';
+            const formData = {
                 grant_type: 'authorization_code',
                 client_id: clientId || config.HUBSPOT_CLIENT_ID,
                 client_secret: clientSecret || config.HUBSPOT_CLIENT_SECRET,
                 redirect_uri: `${config.OAUTH_REDIRECT_BASE}/hubspot`,
                 code: code,
             };
-            let result = await axios({
+            const result = await axios({
                 method: 'post',
                 url: url,
                 data: qs.stringify(formData),
@@ -41,7 +41,7 @@ class HubspotAuthHandler extends BaseOAuthHandler {
                 },
             });
             logInfo('OAuth creds for hubspot', result.data);
-            let info = await axios({
+            const info = await axios({
                 method: 'get',
                 url: 'https://api.hubapi.com/oauth/v1/access-tokens/' + result.data.access_token,
             });
