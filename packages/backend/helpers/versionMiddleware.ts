d945631cd2edb523b489774505f8444c4e99b439
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-02-28 14:32:57 +0000

    Update README - adding new feature

--- packages/backend/helpers/versionMiddleware.ts
+++ packages/backend/helpers/versionMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 type API_VERSIONS = 'v1' | 'v2' | 'latest';
 
-let DEFAULT_API_VERSION: API_VERSIONS = 'v1';
+const DEFAULT_API_VERSION: API_VERSIONS = 'v1';
 
 declare global {
     namespace Express {
@@ -12,8 +12,8 @@ declare global {
     }
 }
 
-let versionMiddleware = () => async (req: Request, _res: Response, next: () => any) => {
-    let version = (req.headers['x-api-version'] as API_VERSIONS) || DEFAULT_API_VERSION;
+const versionMiddleware = () => async (req: Request, _res: Response, next: () => any) => {
+    const version = (req.headers['x-api-version'] as API_VERSIONS) || DEFAULT_API_VERSION;
     req.version = version;
     next();
 };
@@ -22,10 +22,10 @@ type VersionMap = {
     [k in API_VERSIONS]?: any;
 };
 
-export let manageRouterVersioning = (versionMap: VersionMap) => {
+export const manageRouterVersioning = (versionMap: VersionMap) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        let { version } = req;
-        let fn = versionMap[version] || versionMap[DEFAULT_API_VERSION]; // call the v1 function as default
+        const { version } = req;
+        const fn = versionMap[version] || versionMap[DEFAULT_API_VERSION]; // call the v1 function as default
         fn.call(this, req, res, next);
     };
 };
