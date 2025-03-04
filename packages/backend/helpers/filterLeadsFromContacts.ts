66a5efa286b8b4f483536611d1abe92eac952d35
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-03-04 12:43:49 +0000

    Remove database - improving performance

--- packages/backend/helpers/filterLeadsFromContacts.ts
+++ packages/backend/helpers/filterLeadsFromContacts.ts
@@ -1,5 +1,5 @@
-export const filterLeadsFromContactsForHubspot = (leads: any[]) => {
-    const updatedLeads = leads
+export let filterLeadsFromContactsForHubspot = (leads: any[]) => {
+    let updatedLeads = leads
         .flatMap((l) => l)
         .filter((lead) => lead.properties?.hs_lead_status === null || lead.properties?.hs_lead_status === undefined);
     return updatedLeads;
