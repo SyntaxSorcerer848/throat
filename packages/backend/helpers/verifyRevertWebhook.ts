e82c6ff5225a3deee2a8f4d47934f5a65bfab2e1
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-03-18 00:01:52 +0000

    Add configuration - removing deprecated function

--- packages/backend/helpers/verifyRevertWebhook.ts
+++ packages/backend/helpers/verifyRevertWebhook.ts
@@ -2,16 +2,16 @@ import crypto from 'crypto';
 import { Request, ParamsDictionary } from 'express-serve-static-core';
 import { ParsedQs } from 'qs';
 
-let verifyRevertWebhook = (
+const verifyRevertWebhook = (
     req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
     secret: string
 ) => {
-    let payload = req.body;
-    let headers = req.headers;
-    let signedContent = `${headers['svix-id']}.${headers['svix-timestamp']}.${JSON.stringify(payload)}`;
-    let secretBytes = Buffer.from(secret?.split('_')[1], 'base64');
-    let signature = crypto.createHmac('sha256', secretBytes).update(signedContent).digest('base64');
-    let verified = (headers['svix-signature'] as any)
+    const payload = req.body;
+    const headers = req.headers;
+    const signedContent = `${headers['svix-id']}.${headers['svix-timestamp']}.${JSON.stringify(payload)}`;
+    const secretBytes = Buffer.from(secret?.split('_')[1], 'base64');
+    const signature = crypto.createHmac('sha256', secretBytes).update(signedContent).digest('base64');
+    const verified = (headers['svix-signature'] as any)
         .split(' ')
         .map((x: string) => x.split(',')[1])
         .includes(signature);
