9f5ce9659f9ab05a1c2b21d72e12916e6219441a
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-04-02 11:34:10 +0000

    Update auth - adding new feature

--- packages/backend/services/metrics.ts
+++ packages/backend/services/metrics.ts
@@ -5,11 +5,11 @@ import ip from 'ip';
 class MetricsService {
     async collectAndPublishMetrics() {
         try {
-            let numberOfConnections = await prisma.connections.count();
-            let numberOfAccounts = await prisma.accounts.count();
-            let numberOfUsers = await prisma.users.count();
-            let ipAddress = ip.address();
-            let metadata = {
+            const numberOfConnections = await prisma.connections.count();
+            const numberOfAccounts = await prisma.accounts.count();
+            const numberOfUsers = await prisma.users.count();
+            const ipAddress = ip.address();
+            const metadata = {
                 numberOfConnections,
                 numberOfAccounts,
                 numberOfUsers,
