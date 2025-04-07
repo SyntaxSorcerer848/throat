70315f31dd8d92e1ab2400d448c3ee17d42454fe
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-04-07 23:35:28 +0000

    Improve auth - handling edge cases

--- packages/revert-next/hooks/useRecentApiCall.ts
+++ packages/revert-next/hooks/useRecentApiCall.ts
@@ -3,21 +3,21 @@ import { ZodError } from '@revertdotdev/utils';
 import { RecentApiCallSchema, recentApiCallSchema } from '@revertdotdev/types/schemas/recentApiCall';
 import { environmentConfig } from '@revertdotdev/lib/config';
 
-let { REVERT_BASE_API_URL } = environmentConfig;
+const { REVERT_BASE_API_URL } = environmentConfig;
 
 export function useRecentApiCall(appId: string) {
-    let privateToken = localStorage.getItem('privateToken') as string;
+    const privateToken = localStorage.getItem('privateToken') as string;
 
-    let { data, error, isLoading, mutate, isValidating } = useSWR<RecentApiCallSchema>(
+    const { data, error, isLoading, mutate, isValidating } = useSWR<RecentApiCallSchema>(
         `${REVERT_BASE_API_URL}/internal/request/app`,
         async () => {
-            let response = await fetch(`${REVERT_BASE_API_URL}/internal/request/app/${appId}`, {
+            const response = await fetch(`${REVERT_BASE_API_URL}/internal/request/app/${appId}`, {
                 method: 'GET',
                 headers: { 'Content-Type': 'application/json', 'x-revert-api-token': privateToken },
             });
 
-            let jsonReponse = await response.json();
-            let { data, success, error } = recentApiCallSchema.safeParse(jsonReponse);
+            const jsonReponse = await response.json();
+            const { data, success, error } = recentApiCallSchema.safeParse(jsonReponse);
 
             if (!success) {
                 throw new ZodError(error.errors);
