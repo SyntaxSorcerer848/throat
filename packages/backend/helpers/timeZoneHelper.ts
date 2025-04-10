5c6da97cda0d80a3464e87e4c8b54f3bf6589218
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-04-10 04:35:21 +0000

    Improve database - enhancing logs

--- packages/backend/helpers/timeZoneHelper.ts
+++ packages/backend/helpers/timeZoneHelper.ts
@@ -1,69 +1,69 @@
-var getFormattedDate = (inputDate: string) => {
-    var dateObj = new Date(inputDate);
+const getFormattedDate = (inputDate: string) => {
+    const dateObj = new Date(inputDate);
 
     // Extract year, month, and day
-    var year = dateObj.getFullYear();
-    var month = String(dateObj.getMonth() + 1).padStart(2, '0'); // Adding 1 because months are zero-indexed
-    var day = String(dateObj.getDate()).padStart(2, '0');
+    const year = dateObj.getFullYear();
+    const month = String(dateObj.getMonth() + 1).padStart(2, '0'); // Adding 1 because months are zero-indexed
+    const day = String(dateObj.getDate()).padStart(2, '0');
 
     // Format the date as "YYYY-MM-DD"
-    var formattedDate = `${year}-${month}-${day}`;
+    const formattedDate = `${year}-${month}-${day}`;
 
     return formattedDate;
 };
 
-var getDuration = (startDateTime: string, endDateTime: string) => {
-    var startDate = new Date(startDateTime).getTime();
-    var endDate = new Date(endDateTime).getTime();
+const getDuration = (startDateTime: string, endDateTime: string) => {
+    const startDate = new Date(startDateTime).getTime();
+    const endDate = new Date(endDateTime).getTime();
 
     // Calculate the absolute difference in milliseconds
-    var durationMs = Math.abs(endDate - startDate);
+    const durationMs = Math.abs(endDate - startDate);
 
     // Convert milliseconds to hours and minutes
-    var hours = Math.floor(durationMs / (1000 * 60 * 60));
-    var minutes = Math.floor((durationMs % (1000 * 60 * 60)) / (1000 * 60));
+    const hours = Math.floor(durationMs / (1000 * 60 * 60));
+    const minutes = Math.floor((durationMs % (1000 * 60 * 60)) / (1000 * 60));
 
     // Format the duration as "HH:MM"
-    var formattedDuration = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    const formattedDuration = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
 
     return formattedDuration;
 };
 
-var convertToHHMMInUTC = (dateTimeString: string) => {
-    var date = new Date(dateTimeString);
+const convertToHHMMInUTC = (dateTimeString: string) => {
+    const date = new Date(dateTimeString);
 
     // Get hours and minutes in UTC format
-    var hoursUTC = date.getUTCHours().toString().padStart(2, '0');
-    var minutesUTC = date.getUTCMinutes().toString().padStart(2, '0');
+    const hoursUTC = date.getUTCHours().toString().padStart(2, '0');
+    const minutesUTC = date.getUTCMinutes().toString().padStart(2, '0');
 
     // Format the time as "HH:MM"
-    var formattedTimeUTC = `${hoursUTC}:${minutesUTC}`;
+    const formattedTimeUTC = `${hoursUTC}:${minutesUTC}`;
 
     return formattedTimeUTC;
 };
 
-var getCurrentLocale = () => {
-    var dateTimeFormat = new Intl.DateTimeFormat();
-    var options = dateTimeFormat.resolvedOptions();
+const getCurrentLocale = () => {
+    const dateTimeFormat = new Intl.DateTimeFormat();
+    const options = dateTimeFormat.resolvedOptions();
     return options.locale;
 };
 
-var getDateWithShortMonth = () => {
-    var today = new Date();
-    var formattedDate = today.toLocaleDateString(getCurrentLocale(), { day: 'numeric', month: 'short' });
+const getDateWithShortMonth = () => {
+    const today = new Date();
+    const formattedDate = today.toLocaleDateString(getCurrentLocale(), { day: 'numeric', month: 'short' });
     return formattedDate;
 };
 
 function getLastSevenDays() {
-    var daysArray = [];
-    var today = new Date();
+    const daysArray = [];
+    const today = new Date();
 
     for (let i = 6; i > -1; --i) {
-        var currentDate = new Date();
+        const currentDate = new Date();
         currentDate.setDate(today.getDate() - i);
 
         // Formatting the date in form:  "22 Jan"
-        var formattedDate = currentDate.toLocaleDateString(getCurrentLocale(), { day: 'numeric', month: 'short' });
+        const formattedDate = currentDate.toLocaleDateString(getCurrentLocale(), { day: 'numeric', month: 'short' });
 
         daysArray.push(formattedDate);
     }
