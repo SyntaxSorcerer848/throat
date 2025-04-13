d04fe39e828acb709234f6d35a7cf60e9593b0dc
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-04-13 15:53:45 +0000

    Revert auth - fixing a bug

--- packages/backend/helpers/rateLimitMiddleware.ts
+++ packages/backend/helpers/rateLimitMiddleware.ts
@@ -8,15 +8,15 @@ import config from '../config';
 // In Memory Cache for storing RateLimiterRedis instances to prevent the creation of a new instance for each request.
 // The cache key is the 'rate_limit' value derived from the subscriptions table. Currently, we cache by the 'rate_limit' value for simplicity.
 // Using 'subscriptionId' as a key would be more precise but would add complexity in keeping the cache in sync with the database.
-const rateLimiters = new Map<number, RateLimiterRedis>();
+let rateLimiters = new Map<number, RateLimiterRedis>();
 
 //We can make this dynamic based on the subscription as well
-const RATE_LIMIT_DURATION_IN_MINUTES = 1;
-const FALL_BACK_DEFAULT_RATE_LIMIT = 100;
+let RATE_LIMIT_DURATION_IN_MINUTES = 1;
+let FALL_BACK_DEFAULT_RATE_LIMIT = 100;
 
-const getRateLimiter = (rateLimit: number): RateLimiterRedis => {
+let getRateLimiter = (rateLimit: number): RateLimiterRedis => {
     if (!rateLimiters.has(rateLimit)) {
-        const opts: IRateLimiterStoreOptions = {
+        let opts: IRateLimiterStoreOptions = {
             storeClient: redis,
             points: rateLimit, // Points represent the maximum number of requests allowed within the set duration.
             duration: RATE_LIMIT_DURATION_IN_MINUTES * 60, // Converts minutes to seconds for the duration.
@@ -26,18 +26,18 @@ const getRateLimiter = (rateLimit: number): RateLimiterRedis => {
     return rateLimiters.get(rateLimit)!;
 };
 
-const rateLimitMiddleware = () => async (req: Request, res: Response, next: Function) => {
+let rateLimitMiddleware = () => async (req: Request, res: Response, next: Function) => {
     if (skipRateLimitRoutes(req)) {
         return next();
     }
     try {
-        const { 'x-revert-t-id': tenantId } = req.headers;
-        const { subscription, id: accountId } = res.locals.account || {}; // Subscription details are retrieved from response locals set earlier in the revertAuthMiddleware.
-        const rateLimit =
+        let { 'x-revert-t-id': tenantId } = req.headers;
+        let { subscription, id: accountId } = res.locals.account || {}; // Subscription details are retrieved from response locals set earlier in the revertAuthMiddleware.
+        let rateLimit =
             subscription?.rate_limit ?? config.DEFAULT_RATE_LIMIT_DEVELOPER_PLAN ?? FALL_BACK_DEFAULT_RATE_LIMIT; //incase subscription undefined, we will use the default rate limit this is to make sure backward compatibility as currently some accounts might not have subscription attached to them. We can remove the optional chaining and nullish coalescing once we are sure that all accounts have subscription attached to them. In case of DEFAULT_RATE_LIMIT_DEVELOPER_PLAN is missing in config, we will fallback to 1
-        const rateLimiter = getRateLimiter(rateLimit);
+        let rateLimiter = getRateLimiter(rateLimit);
         //added accountId to make the key unique
-        const uniqueKey = `${accountId}-${tenantId}`;
+        let uniqueKey = `${accountId}-${tenantId}`;
         // rate limit is per tenantId
         await rateLimiter.consume(uniqueKey, 1); // consume 1 point for each request
         next();
