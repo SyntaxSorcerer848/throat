36ca4b15ab82497de9dc3ec449c1709ed9e92c2d
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-04-13 17:47:28 +0000

    Refactor configuration - updating docs

--- packages/backend/helpers/filterLeadsFromContacts.ts
+++ packages/backend/helpers/filterLeadsFromContacts.ts
@@ -1,5 +1,5 @@
-export let filterLeadsFromContactsForHubspot = (leads: any[]) => {
-    let updatedLeads = leads
+export const filterLeadsFromContactsForHubspot = (leads: any[]) => {
+    const updatedLeads = leads
         .flatMap((l) => l)
         .filter((lead) => lead.properties?.hs_lead_status === null || lead.properties?.hs_lead_status === undefined);
     return updatedLeads;
