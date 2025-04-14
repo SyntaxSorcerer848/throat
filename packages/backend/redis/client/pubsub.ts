757156845626a33045812b7a781ba8258a81c515
SyntaxSorcerer848 <zaqqq12341@yahoo.com>
2025-04-14 12:13:51 +0000

    Revert tests - handling edge cases

--- packages/backend/redis/client/pubsub.ts
+++ packages/backend/redis/client/pubsub.ts
@@ -1,23 +1,23 @@
 import config from '../../config';
 import { createRedisClient } from '.';
 
-let subscribe = async (channelName: string, callback: any) => {
-    let subscriber = createRedisClient(config.REDIS_URL);
+const subscribe = async (channelName: string, callback: any) => {
+    const subscriber = createRedisClient(config.REDIS_URL);
     await subscriber.connect();
     await subscriber.subscribe(channelName, callback);
 };
 
-let publish = async (channelName: string, data: any) => {
-    let publisher = createRedisClient(config.REDIS_URL);
+const publish = async (channelName: string, data: any) => {
+    const publisher = createRedisClient(config.REDIS_URL);
     await publisher.connect();
     await publisher.publish(channelName, JSON.stringify(data));
 };
 
-let pubsub = { publish, subscribe };
+const pubsub = { publish, subscribe };
 
 export default pubsub;
 
-export let PUBSUB_CHANNELS = {
+export const PUBSUB_CHANNELS = {
     INTEGRATION_STATUS: 'integrationStatus',
 };
 
